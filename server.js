ddae2a32ba1da8d5ec6920686f4f524e56463552
senguttuvan s <scheran245@gmail.com>
2018-05-07 08:13:57 +0800

    intial commit- template engine hbs,using partials in hbs,registerPartials,registerhelper, serving static files etc...

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require ('express');
+const path = require ('path');
+const hbs = require ('hbs');
+
+
+var app=express();
+
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getcurrentYear',()=>{
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper('welcomeMsg',(txt)=>{
+    return txt.toUpperCase();
+})
+
+app.get('/',(req,res) => {
+    res.render('home.hbs',{msg:'home'});
+})
+app.get('/about',(req,res) => {
+    res.render('about.hbs',{msg:'about'});
+})
+
+app.listen(3000,()=>{
+    console.log('server upon port 3000!!');
+});
+
